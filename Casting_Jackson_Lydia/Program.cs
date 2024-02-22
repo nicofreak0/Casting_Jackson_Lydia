d46d44327a64453641cc5ca27d6197775cdd3f93
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-02-22 13:55:04 -0600

    comments

--- Casting_Jackson_Lydia/Program.cs
+++ Casting_Jackson_Lydia/Program.cs
@@ -4,14 +4,19 @@
     {
         static void Main(string[] args)
         {
+            // asks the user to enter a whole number
             Console.WriteLine("Enter a Whole Number");
+            // assigns input to integer
             int myFavNum = Convert.ToInt32(Console.ReadLine());
 
+            // prints favorite number to console
             Console.WriteLine("Your Favorite Number is " + myFavNum);
 
+            // asks user true or false question and assigns response to boolean
             Console.WriteLine("Do You Like Cats? (please enter 'true' or 'false')");
             bool isTrue = Convert.ToBoolean(Console.ReadLine());
 
+            // print the result of the question being asked
             Console.WriteLine($"It is {isTrue} that I like cats!");
 
 
