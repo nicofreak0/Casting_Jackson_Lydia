139eac6fefd5d09a89746d7a4aff519b2549627f
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-02-22 13:45:30 -0600

    eneter

--- Casting_Jackson_Lydia/Program.cs
+++ Casting_Jackson_Lydia/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a Whole Number");
+            int myFavNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your Favorite Number is " + myFavNum);
+
+            Console.WriteLine("Do You Like Cats? (please enter 'true' or 'false'");
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine($"It is {isTrue} that I like cats!");
+
+
         }
     }
 }
